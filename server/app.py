0f8d0aa8365e7e9888f1ec3f493c5b1675ba9968
JingSyue <acs108801@gm.ntcu.edu.tw>
2024-03-24 16:57:16 +0800

    add flask cors

--- server/app.py
+++ server/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import game_logic
 import uuid
 
 app = Flask(__name__)
+CORS(app)
+
 @app.route('/', methods=['GET'])
 def game_intro():
     return jsonify({"message": "Welcome to 24 point game"})
